536f4322ce8b243663f03eab567680da988aa113
Aldrin Cunanan <cunanan.aldrin.t@gmail.com>
2023-01-15 13:13:23 +0800

    remove 2d array, it is not needed

--- src/table.ts
+++ src/table.ts
@@ -1,18 +1,9 @@
 export default class Table {
-	public table: Number[][];
 	public width: number;
 	public height: number;
 
 	constructor(width: number = 5, height: number = width) {
 		this.width = width;
 		this.height = height;
-
-		const x = Array(width);
-		const y = new Array(height);
-
-		y.fill(0);
-		x.fill(y);
-
-		this.table = x;
 	}
 }
